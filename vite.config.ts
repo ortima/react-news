1d246c9046a1f70799b6ba4b8782375ee4015b4c
ortima <account.net@mail.ru>
2024-07-10 00:53:09 +0300

    feat: added aliases (helpers, pages, api)

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
+      "@helpers": path.resolve(__dirname, "./src/helpers"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@api": path.resolve(__dirname, "./src/api"),
     },
   },
 });
