7106f2ac22d3ef96feacdee4469cb77daad6a968
ortima <account.net@mail.ru>
2024-07-10 23:11:33 +0300

    feat: add skeleton

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import { formatDate } from "@helpers/formatDate";
 const Header = () => {
   return (
     <header className="flex w-full flex-col gap-3 border-b-4 border-b-[#F2F4F5] bg-white p-6 py-3">
-      <h1 className="text-2xl">News by ORTIMA</h1>
+      <h1 className="font-serif text-2xl font-semibold uppercase">
+        News by ORTIMA
+      </h1>
       <p className="text-base">{formatDate(new Date())}</p>
     </header>
   );
