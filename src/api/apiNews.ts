a561dbf7cdc02b363d0e59bed19595ec636cf892
ortima <account.net@mail.ru>
2024-07-13 18:26:01 +0300

    feat: add pagination

--- src/api/apiNews.ts
+++ src/api/apiNews.ts
@@ -4,14 +4,14 @@ import { GetNewsResponse } from "src/@types";
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (): Promise<GetNewsResponse> => {
+export const getNews = async (
+  page_number = 1,
+  page_size = 10,
+): Promise<GetNewsResponse> => {
   try {
-    const response = await axios.get<GetNewsResponse>(
-      `${BASE_URL}latest-news`,
-      {
-        params: { apiKey: API_KEY },
-      },
-    );
+    const response = await axios.get<GetNewsResponse>(`${BASE_URL}search`, {
+      params: { apiKey: API_KEY, page_number, page_size },
+    });
     return response.data;
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
