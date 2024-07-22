c10de00e54c9f66cfa4126e42b0507c24d7a9b93
ortima <account.net@mail.ru>
2024-07-22 23:34:51 +0300

    fix: reform code

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -3,14 +3,12 @@ import { withSkeleton } from "@helpers/hocs/withSkeleton";
 import { News } from "src/@types";
 
 interface INewsList {
-  news: News[];
+  news: News[] | null;
 }
 const NewsList: React.FC<INewsList> = ({ news }) => {
   return (
     <ul className="flex w-full flex-col gap-6">
-      {news.map((item) => (
-        <NewsItem key={item.id} item={item} />
-      ))}
+      {news?.map((item) => <NewsItem key={item.id} item={item} />)}
     </ul>
   );
 };
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,52 +1,13 @@
-import { useCallback } from "react";
-import { getNews } from "@api/apiNews.ts";
 import LatestNews from "@components/LatestNews/LatestNews";
 import NewsByFilters from "@components/NewsByFilters/NewsByFilters";
-import { PAGE_SIZE } from "@constants/constants.ts";
-import { useDebounce } from "@helpers/hooks/useDebounce.ts";
-import { useFetch } from "@helpers/hooks/useFetch.ts";
-import { useFilters } from "@helpers/hooks/useFilters.ts";
-import { Filters, GetNewsResponse } from "src/@types";
 import styles from "./styles.module.css";
 
 const Main: React.FC = () => {
-  const { filters, changeFilter } = useFilters<Filters>({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
-
-  const debouncedKeywords = useDebounce(filters.keywords, 1500);
-
-  const fetchNews = useCallback(
-    () =>
-      getNews({
-        page_number: filters.page_number,
-        page_size: filters.page_size,
-        category: filters.category || undefined,
-        keywords: debouncedKeywords,
-      }),
-    [
-      filters.page_number,
-      filters.page_size,
-      filters.category,
-      debouncedKeywords,
-    ],
-  );
-
-  const { data, isLoading } = useFetch<GetNewsResponse>(fetchNews);
-
   return (
     <main className={styles.main}>
-      <LatestNews isLoading={isLoading} banners={data ? data.news : []} />
+      <LatestNews />
 
-      <NewsByFilters
-        news={data ? data.news : []}
-        isLoading={isLoading}
-        filters={filters}
-        changeFilter={changeFilter}
-      />
+      <NewsByFilters />
     </main>
   );
 };
